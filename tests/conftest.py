5dd5c49235f3ace0297835b5faceffd0093134c8
Guru Prasad Reddy K <kguruprasadreddy2004@gmail.com>
2025-03-01 17:04:50 +0530

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-# tests/conftest.py
-import sys
-import os
-
-# Add the project root (one level above the tests folder) to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
